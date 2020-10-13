ad29930a697bd96f1819107d80dfa5d26ebfdcc9
Taniya Shaffer <shaffertaniya@gmail.com>
2020-10-12 20:05:57 -0400

    Final commit of functional shape class

--- Shaffer_p3/src/Pyramid.java
+++ Shaffer_p3/src/Pyramid.java
@@ -13,13 +13,13 @@ public class Pyramid extends Shape3D {
     @Override
     public double getArea() {
         double pyramidSurfaceArea;
-        pyramidSurfaceArea = (base * width) + (base * Math.sqrt((width / 2) * (width / 2) + height * height)) + (width * Math.sqrt(((base / 2) * (base / 2)) + height * height));       // SA = (B*W) + L*sqrt[(W/2)^2 + H^2] + W*sqrt[(B/2)^2 + H^2]
+        pyramidSurfaceArea = (base * width) + (base * Math.sqrt((width / 2) * (width / 2) + height * height)) + (width * Math.sqrt(((base / 2) * (base / 2)) + height * height));       // SA = (base * width) + base * sqrt[(width/2)^2 + height^2] + width*sqrt[(base/2)^2 + height^2]
         return pyramidSurfaceArea;
     }
     @Override
     public double getVolume() {
         double pyramidVolume;
-        pyramidVolume = (base * width * height) / 3;
+        pyramidVolume = (base * width * height) / 3;        // V = (base * width * height) / 3
         return pyramidVolume;
     }
 }
